d6e48cf4050bf59ef0801313c01672968c354f34
yingzhuo <yingzhor@gmail.com>
2020-09-06 16:34:59 +0800

    update

--- custom/plugins/my/my.plugin.zsh
+++ custom/plugins/my/my.plugin.zsh
@@ -1,18 +1,43 @@
 #!/usr/bin/env zsh
 
-# ubuntu
-alias update=update_ubuntu
-
-update_ubuntu() {
+#-----------------------------------------------------------------------------------------------------------------------
+# Ubuntu
+#-----------------------------------------------------------------------------------------------------------------------
+alias ubuntu.update=ubuntu_update
+ubuntu_update() {
   sudo apt-get update -y
   sudo apt-get upgrade -y
   sudo apt-get dist-upgrade -y
   sudo apt-get autoremove -y
 }
 
-# docker
-alias docker-clean=docker_clean
+#-----------------------------------------------------------------------------------------------------------------------
+# System
+#-----------------------------------------------------------------------------------------------------------------------
+alias sys.reboot=sys_reboot
+sys_reboot() {
+  sudo reboot
+}
 
+alias sys.shutdown=sys_shutdown
+sys_shutdown() {
+  sudo shutdown now
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Docker
+#-----------------------------------------------------------------------------------------------------------------------
+alias docker.clean=docker_clean
 docker_clean() {
+  sudo docker system prune -f
+}
+
+alias docker.clean.force=docker_clean_force
+docker_clean_force() {
   sudo docker system prune -a -f
+}
+
+alias docker.restart=docker_restart
+docker_restart() {
+  sudo systemctl restart docker.service
 }
