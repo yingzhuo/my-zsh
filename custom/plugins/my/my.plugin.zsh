fd65b4bdbd7a7d70d298b3bf2a86cc7c89f8bde2
yingzhuo <yingzhor@gmail.com>
2020-09-08 09:00:52 +0800

    update

--- custom/plugins/my/my.plugin.zsh
+++ custom/plugins/my/my.plugin.zsh
@@ -3,16 +3,20 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # System
 #-----------------------------------------------------------------------------------------------------------------------
+
+# 重启机器
 alias sys.reboot=sys_reboot
 sys_reboot() {
   sudo reboot
 }
 
+# 关闭机器
 alias sys.shutdown=sys_shutdown
 sys_shutdown() {
   sudo shutdown now
 }
 
+# 更新系统
 alias sys.update=ubuntu_update
 ubuntu_update() {
   sudo apt-get update -y
@@ -21,24 +25,35 @@ ubuntu_update() {
   sudo apt-get autoremove -y
 }
 
+# 显示系统信息
 alias sys.info=ubuntu_info
 ubuntu_info() {
   lsb_release -a
 }
 
+# 同步时间
+alias sys.ntp.update=ntp_update
+ntp_update() {
+  sudo ntpdate -u cn.pool.ntp.org
+}
+
 #-----------------------------------------------------------------------------------------------------------------------
 # Docker
 #-----------------------------------------------------------------------------------------------------------------------
+
+# 清理docker
 alias docker.clean=docker_clean
 docker_clean() {
   sudo docker system prune -f
 }
 
+# 强制清理docker
 alias docker.clean.force=docker_clean_force
 docker_clean_force() {
   sudo docker system prune -a -f
 }
 
+# 重启docker服务
 alias docker.restart=docker_restart
 docker_restart() {
   sudo systemctl restart docker.service
